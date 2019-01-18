08c0d9e913941795735cb4ed940497e5c59a77b8
jk <jk.kuklis@gmail.com>
2019-01-18 23:58:28 +0100

    Last fixes

--- interpreter/src/pl/edu/mimuw/cloudatlas/client/handlers/SetHostHandler.java
+++ interpreter/src/pl/edu/mimuw/cloudatlas/client/handlers/SetHostHandler.java
@@ -2,11 +2,16 @@ package pl.edu.mimuw.cloudatlas.client.handlers;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Map;
 
 import pl.edu.mimuw.cloudatlas.agent.utility.Logger;
 import pl.edu.mimuw.cloudatlas.client.Client;
 import pl.edu.mimuw.cloudatlas.client.ClientStructures;
+import pl.edu.mimuw.cloudatlas.cloudAtlasAPI.CloudAtlasAPI;
 
 public class SetHostHandler extends RMIHandler {
     public SetHostHandler(ClientStructures structures) {
@@ -19,15 +24,28 @@ public class SetHostHandler extends RMIHandler {
         if (!parameters.containsKey("host")) {
             response = "Error: host not specified!";
         } else {
+            String prevHost = structures.getHost();
             String newHost = parameters.get("host");
             try {
                 logger.log("Trying to connect to host: " + newHost);
                 InetAddress ia = InetAddress.getByName(newHost);
                 structures.setHost(newHost);
-                Client.rebindCloudAtlas(structures, logger);
+                Registry registry = LocateRegistry.getRegistry(structures.getHost());
+                structures.cloudAtlas = (CloudAtlasAPI) registry.lookup("CloudAtlasAPI");
+                logger.log("CloudAtlas bound");
                 response = "Host set to " + newHost;
+
             } catch (UnknownHostException e) {
+                structures.setHost(prevHost);
                 response = "Error: host unreachable!";
+
+            } catch (RemoteException e) {
+                structures.setHost(prevHost);
+                response = "Error: remote exception!";
+
+            } catch (NotBoundException e) {
+                structures.setHost(prevHost);
+                response = "Error: CloudAtlas not bound!";
             }
         }
         logger.log(response);
